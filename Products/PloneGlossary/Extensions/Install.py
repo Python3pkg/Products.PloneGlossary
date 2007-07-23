a13a4c367b169ac0fa01f3acd94ecd462d8d9bd8
b_mathieu <b_mathieu@localhost>
2007-07-23 15:01:25 +0000

    Install: check for scripts/CSS before registering them
    svn path=/PloneGlossary/trunk/; revision=46120

--- Products/PloneGlossary/Extensions/Install.py
+++ Products/PloneGlossary/Extensions/Install.py
@@ -20,7 +20,7 @@ try:
     from Products.CMFCore.permissions import ManagePortal
 except:
     from Products.CMFCore.CMFCorePermissions import ManagePortal
-    
+
 # Archetypes
 from Products.Archetypes.Extensions.utils import installTypes, install_subskin
 from Products.Archetypes.public import listTypes
@@ -40,9 +40,9 @@ from Products.PloneGlossary.types.PloneGlossary import PloneGlossary
 
 def install_tool(self, out):
     """Install PloneGlossary tool"""
-    
+
     tool = getToolByName(self, PLONEGLOSSARY_TOOL, None)
-    
+
     if tool is None:
         #Add a tool to manage vocabulary
         add_tool = self.manage_addProduct[PROJECTNAME].manage_addTool
@@ -57,32 +57,32 @@ glossary_portlets = ('here/portlet_ploneglossary/macros/portlet',)
 
 def install_portlet(self, out):
     """Install portlet in right slots"""
-    
+
     utool = getToolByName(self, 'portal_url')
     portal = utool.getPortalObject()
     portlets = list(portal.getProperty('right_slots'))
-    
+
     for portlet in glossary_portlets:
         if portlet not in portlets:
             portlets.append(portlet)
-    
+
     portal._updateProperty('right_slots', portlets)
- 
+
     print >>out, 'PloneGlossary portlet installed.\n'
 
 def uninstall_portlet(self, out):
     """Install portlet in right slots"""
-    
+
     utool = getToolByName(self, 'portal_url')
     portal = utool.getPortalObject()
     portlets = list(portal.getProperty('right_slots'))
-    
+
     for portlet in glossary_portlets:
         if portlet in portlets:
             portlets.remove(portlet)
-    
+
     portal._updateProperty('right_slots', portlets)
-    
+
     print >>out, 'PloneGlossary portlet installed.\n'
 
 def install_types(self, out):
@@ -93,28 +93,32 @@ def install_types(self, out):
 
     ftool = getToolByName(self, 'portal_factory')
     types_to_add = (
-        'PloneGlossary', 
+        'PloneGlossary',
         'PloneGlossaryDefinition',)
     ftypes = ftool.getFactoryTypes()
     ftypes.update(dict([(x, 1) for x in types_to_add]))
     ftool.manage_setPortalFactoryTypes(listOfTypeIds=ftypes.keys())
     out.write("Types configured to use portal_factory\n")
-    
+
 
 def install_scripts(self, out):
     """Install javascripts and CSS in their respective registry"""
     expr = "python:portal.portal_glossary.showPortlet() "\
            "or portal.portal_glossary.highlightContent(here)"
     jscript_reg = getToolByName(self, 'portal_javascripts')
-    
-    jscript_reg.registerScript(id="ploneglossary.js", expression=expr)
-    jscript_reg.registerScript(id="ploneglossary_definitions.js",
-                               expression=expr,
-                               inline=True,)
+    installed = jscript_reg.getResourceIds()
+    if 'ploneglossary.js' not in installed:
+        jscript_reg.registerScript(id="ploneglossary.js", expression=expr)
+    if 'ploneglossary_definitions.js' not in installed:
+        jscript_reg.registerScript(id="ploneglossary_definitions.js",
+                                   expression=expr,
+                                   inline=True,)
 
     css_reg = getToolByName(self, 'portal_css')
-    css_reg.registerStylesheet('ploneglossary_popup.css',
-                               title='Plone Glossary')
+    installed = css_reg.getResourceIds()
+    if 'ploneglossary_popup.css' not in installed:
+        css_reg.registerStylesheet('ploneglossary_popup.css',
+                                   title='Plone Glossary')
     print >> out, "registered CSS and Javascript into their respective registry"
 
 def install(self):
@@ -125,42 +129,42 @@ def install(self):
 
     # Install tools
     install_tool(self, out)
-    
+
     # Install skin
     install_subskin(self, out, GLOBALS)
 
     # register scripts
     install_scripts(self, out)
-    
+
     # Install portlet in right slots
     install_portlet(self, out)
-    
+
     # Install configlet
     cp_tool = getToolByName(self, 'portal_controlpanel')
     cp_tool.registerConfiglet(**ploneglossary_prefs_configlet)
-    
+
     registerGlossary(self, PloneGlossary, out)
     if DEBUG or os.environ.get(INSTALL_EXAMPLE_TYPES_ENVIRONMENT_VARIABLE):
         from Products.PloneGlossary.examples.exampleglossary \
                 import ExampleGlossary
         registerGlossary(self, ExampleGlossary, out)
-        
+
     out.write('Installation completed.\n')
     return out.getvalue()
 
 def uninstall(self):
     """Uninstall PloneGlossary product"""
     out = StringIO()
-    
+
     # Uninstall portlet in right slots
     uninstall_portlet(self, out)
-    
+
     # Uninstall configlets
     try:
         cp_tool = getToolByName(self, 'portal_controlpanel')
         cp_tool.unregisterApplication(PROJECTNAME)
     except:
         pass
-    
+
     out.write('Uninstallation completed.\n')
     return out.getvalue()
