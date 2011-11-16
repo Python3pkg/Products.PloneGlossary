2b19d6726eb6af13a7764faf298e7c2cb2fb7404
Maurits van Rees <maurits@vanrees.org>
2011-11-16 16:07:47 +0000

    Cleanup the plone domain translations.
    The existing msgids were not used and others (from portlets.xml) were missing.
    
    svn path=/Products.PloneGlossary/trunk/; revision=246410

--- Products/PloneGlossary/rebuild_i18n.sh
+++ Products/PloneGlossary/rebuild_i18n.sh
@@ -5,3 +5,11 @@
 i18ndude rebuild-pot --pot i18n/glossary.pot --create ploneglossary .
 # Sync with the ploneglossary po files.
 i18ndude sync --pot i18n/glossary.pot $(find i18n -iregex '.*\.po$'|grep -v plone)
+
+# Rebuild the pot file for the plone domain.  We only look in the GS
+# profiles.  Actually, this does not work; I think it wants to find at
+# least one translation in a page template or python file.  So skip this.
+#i18ndude rebuild-pot --pot i18n/glossary-plone.pot --create plone profiles/
+
+# Sync with the plone po files.
+#i18ndude sync --pot i18n/glossary-plone.pot i18n/glossary-plone-*.po
