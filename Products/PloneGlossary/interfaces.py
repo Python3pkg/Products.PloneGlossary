c098008e9487e994b06accaf3dce05eb7e935827
Maurits van Rees <maurits@vanrees.org>
2011-11-23 23:55:57 +0000

    Merged branch maurits-optional-highlight-per-object:
    Added support for switching highlighting on or off per object.  You
    need to write an adapter if you want to use this.  See for example
    https://github.com/zestsoftware/zest.ploneglossaryhighlight
    
    svn path=/Products.PloneGlossary/trunk/; revision=246667

--- Products/PloneGlossary/interfaces.py
+++ Products/PloneGlossary/interfaces.py
@@ -76,3 +76,15 @@ class IPloneGlossaryDefinition(Interface):
 
     def reindexObject(idxs=[]):
         """Reindex object in portal catalog and glossary catalog"""
+
+
+class IOptionalHighLight(Interface):
+    """Do we want to highlight terms in this object?
+    """
+
+    def do_highlight(default):
+        """Highlight this object?
+
+        When no preference can be found, return the default value.
+        This should be optional
+        """
