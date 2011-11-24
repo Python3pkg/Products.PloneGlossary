2f1bffc57df848c3612632f6a0c0d4edc28f4598
Maurits van Rees <maurits@vanrees.org>
2011-11-24 00:02:40 +0000

    Add Plone classifiers in setup.py
    svn path=/Products.PloneGlossary/trunk/; revision=246669

--- setup.py
+++ setup.py
@@ -11,12 +11,16 @@ long_description = long_description.decode('utf8')
 setup(
     name='Products.PloneGlossary',
     version=version,
-    long_description = long_description,
+    long_description=long_description,
     description="Hilite Plone content terms, mouseover shows the term definition as tooltip.",
-    # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+    # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
         "Programming Language :: Python",
         "Framework :: Plone",
+        "Framework :: Plone :: 3.2",
+        "Framework :: Plone :: 3.3",
+        "Framework :: Plone :: 4.0",
+        "Framework :: Plone :: 4.1",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
     keywords='plone glossary',
