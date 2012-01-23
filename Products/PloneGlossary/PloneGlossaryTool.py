56e9e045a534a4e052d4a0707afd3d47e4ac476f
Andreas Jung <zopyxfilter@gmail.com>
2012-01-23 14:57:55 +0000

    fix for brown bag release
    svn path=/Products.PloneGlossary/trunk/; revision=247911

--- Products/PloneGlossary/PloneGlossaryTool.py
+++ Products/PloneGlossary/PloneGlossaryTool.py
@@ -580,8 +580,9 @@ class PloneGlossaryTool(PropertyManager, UniqueObject, SimpleItem):
             glossary_uids = None
         # ensure that terms are sorted in descending term length in order to match
         # terms with the same prefix properly (longest matches first) (ajung)
+        defs = self.getObjectRelatedDefinitions(context, glossary_uids)
         defs.sort(lambda t1,t2: -cmp(len(t1['terms'][0]), len(t2['terms'][0])))
-        return self.getObjectRelatedDefinitions(context, glossary_uids)
+        return defs
 
     security.declarePublic('searchResults')
     def searchResults(self, glossary_uids, **search_args):
