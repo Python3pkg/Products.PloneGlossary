2a4440776d7619654e26e385401e9c26183dca85
Mikel Larreategi <mlarreategi@codesyntax.com>
2013-10-02 11:34:26 +0200

    remove duplicated import

--- Products/PloneGlossary/content/schemata.py
+++ Products/PloneGlossary/content/schemata.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 ##
 ## Copyright (C) 2007 Ingeniweb
-from Products.ATContentTypes.content.folder import ATFolderSchema
 
 ## This program is free software; you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
